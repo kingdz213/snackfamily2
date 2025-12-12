61c00a8ce9dc4fd8d2c04bfaebb97a39972c2834
kingdz213 <gt4nfhqwkg@privaterelay.appleid.com>
2025-12-12 03:55:41 +0100

    firebase.ts

---
+++ firebase.ts
@@ -0,0 +1,22 @@
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+import { getAnalytics, isSupported } from "firebase/analytics";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDWHgqFOblVcyy14qROkGth-gUqCyug0AY",
+  authDomain: "snackfamily2.firebaseapp.com",
+  projectId: "snackfamily2",
+  storageBucket: "snackfamily2.firebasestorage.app",
+  messagingSenderId: "749971984886",
+  appId: "1:749971984886:web:9d9f262fe288178efb77d7",
+  measurementId: "G-CLR14N1PER"
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
+
+// Firestore DB
+export const db = getFirestore(app);
+
+// Analytics (optional)
+export const analytics = await isSupported().then(s => s ? getAnalytics(app) : null);
